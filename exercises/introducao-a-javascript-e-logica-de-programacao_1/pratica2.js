36c5681ba78255edc7bb6c4fc9a342b78b58bcdc
Lucas Brito <lucasbrito.carneiro@gmail.com>
2021-02-24 18:49:54 -0300

    Práticas 2 e 3 completas

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/pratica2.js
@@ -0,0 +1,9 @@
+let num1 = 30;
+let num2 = 20;
+
+if (num1 > num2){
+    console.log(num1);
+}
+else{
+    console.log(num2);
+}
---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/pratica3.js
@@ -0,0 +1,14 @@
+let num1 = 30;
+let num2 = 50;
+let num3 = 10;
+
+if (num1 > num2 && num1 > num3){
+    console.log(num1);
+}
+else if (num2 > num1 && num2 > num3){
+    console.log(num2);
+}
+else{
+    console.log(num3);
+}
+
